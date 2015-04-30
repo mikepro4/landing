f757c546bfa66c024cc65afa5618c918efb9086f
artomatt <mattgordils@gmail.com>
2015-04-30 18:12:53 -0400

    exchange 4

--- app/javascripts/components/Testimonials.jsx
+++ app/javascripts/components/Testimonials.jsx
@@ -12,22 +12,162 @@ var Testimonials = React.createClass({
     router: React.PropTypes.func
   },
 
+
+  getInitialState: function() {
+    return {
+      quotes: [
+        {
+          text: 'CompStak is my go to resource... complete data on lease comparables and is thorough in maintaining their database.',
+          person: 'Scott Amoson',
+          company: 'Colliers International'
+        },
+        {
+          text: 'My valuation teams are continually surprised by both the amount and quality of data available on Compstak Exchange in the Chicagoland area.',
+          person: 'Brandon Nunnink',
+          company: 'Integra Realty Resources'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 1',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 2',
+          person: 'title',
+          company: 'position'
+        },
+        {
+          text: 'Message 3',
+          person: 'title',
+          company: 'position'
+        }
+      ]
+    }
+  },
+
   componentDidMount: function() {
     $('.testimonials-block').cycle({
-        speed: 0,
+        speed: 500,
+        timeout: 0,
         fx: 'carousel',
         carouselFluid: true,
         carouselVisible : 3,
         manualSpeed: 500,
         next : '.next-arrow',
         prev : '.prev-arrow',
-        slides : '> li',
-        swipe : true
-        //slide-active-class : 'active'
+        slides : '.slide',
+        swipe : true,
+        slideActiveClass : 'active'
     });
   },
 
   render: function () {
+
+    var quotes = this.state.quotes.map(function (quote, i) {
+      return (
+        <div className="slide">
+          <p>{quote.text}</p>
+          <p>{quote.person}</p>
+          <p>{quote.company}</p>
+        </div>
+      );
+    }.bind(this));
+
     return (
       <section className={classnames({
         'testimonials': true,
@@ -37,16 +177,11 @@ var Testimonials = React.createClass({
         'blue-bg': (this.props.mode == "blue") ? true : false,
         'white': (this.props.mode == "white") ? true : false
       })}>
-        <ul className="testimonials-block">
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-          <li><p>Compstak is on the bleeding edge of bringing big data and scale in an innovative way to help T3's clients get better deals all across the country.</p></li>
-        </ul>
+
+        <div className="quotes">”</div>
+        <div className="testimonials-block">
+          {quotes}
+        </div>
 
         <div className="prev-arrow">Prev</div>
         <div className="next-arrow">Next</div>
